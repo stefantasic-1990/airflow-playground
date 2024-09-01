d4bfe5188a3271cc532387eae66445b2c09017b5
Stefan Tasic <stefantasic@outlook.com>
2024-09-01 19:50:09 -0400

    connected to the metadata db

---
+++ dags/metadata_manipulator.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+dag = DAG(
+    dag_id = "metadata_manipulator",
+    start_date = days_ago(1),
+    schedule_interval = "*/5 * * * *",
+    catchup = False,
+    default_args = {
+        "owner": "stefantasic",
+        "depends_on_past": False,
+        "retries": 2,
+        "retry_delay": 5.0,
+        "provide_context": True
+        }
+)
+
+run_query = PostgresOperator(
+    task_id='run_query',
+    postgres_conn_id='metadata-db',
+    sql="SELECT 1;",
+    dag=dag
+)
